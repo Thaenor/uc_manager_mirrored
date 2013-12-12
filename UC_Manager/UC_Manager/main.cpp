fe8b95dc61f2311b0778721179a2cd9b6cbb9779
1111315FranciscoSantos <1111315@isep.ipp.pt>
2013-12-12 18:59:08 +0000

    First commit! 1111315 Project UC_Manager. added Main.cpp and .gitignore for Xcode, Visual Studio and C++ files.

---
+++ UC_Manager/UC_Manager/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  UC_Manager
+//
+//  Created by Francisco Santos on 12/12/13.
+//  Copyright (c) 2013 Grupo B6-5. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Welcome to UC Manager!\n";
+    return 0;
+}
+
