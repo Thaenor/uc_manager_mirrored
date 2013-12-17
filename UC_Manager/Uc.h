66edeebd158d496fde145e000b7e8a1fc4545033
1111315FranciscoSantos <1111315@isep.ipp.pt>
2013-12-17 01:04:12 +0000

    1111315: Created Class Uc.h, main menu implemented, created some test objects. TODO: fix overload of operator <<

---
+++ UC_Manager/Uc.h
@@ -0,0 +1,14 @@
+//
+//  Uc.h
+//  UC_Manager
+//
+//  Created by Francisco Santos on 16/12/13.
+//  Copyright (c) 2013 Grupo B6-5. All rights reserved.
+//
+
+#ifndef UC_Manager_Uc_h
+#define UC_Manager_Uc_h
+
+
+
+#endif
