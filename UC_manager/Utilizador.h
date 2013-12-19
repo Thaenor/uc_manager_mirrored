094812c7edb6c3c2f67398836769450755de19e3
Afonso Reis <a.reis_05@hotmail.com>
2013-12-19 03:20:03 +0000

    1090537 - Main alterado

--- UC_manager/Utilizador.h
+++ UC_manager/Utilizador.h
@@ -1,9 +1,2 @@
 
-#ifndef UC_Manager_Utilizador_h
-#define UC_Manager_Utilizador_h
 
-class Utilizador: public Pessoa
-{
-}
-    
-#endif
--- UC_manager/main.cpp
+++ UC_manager/main.cpp
@@ -10,6 +10,7 @@
 #include "Aluno.h"
 #include "Pessoa.h"
 #include "Utilizador.h"
+
 using namespace std;
 
 
@@ -18,34 +19,7 @@ using namespace std;
 /*******************************************************************************************/
 /*******************************************************************************************/
 
-// Criacao de menus
-void menuprincipal();
 
-//Funções para compatibilidade entre Sistemas Operativos
-void clear()
-{
-#ifdef _WIN64
-    system("cls");
-#elif _WIN32
-    system("cls");
-#else
-    cout << string( 100, '\n' );
-#endif
-    
-}
-
-void pause()
-{
-#ifdef _WIN64
-    system("pause");
-#elif _WIN32
-    system("pause");
-#else
-    cout << "paused, press any key to resume.";
-    cin.get();
-    cin.get();
-#endif
-}
 
 /*******************************************************************************************/
 /*******************************************************************************************/
@@ -55,14 +29,14 @@ void pause()
 int main(void)
 {
 
-    // insert code here...
-    //menuprincipal();
+	// insert code here...
+	//menuprincipal();
 
-    cout << "Development test area!" << endl;
+	cout << "Development test area!" << endl;
  
-    
-    cout << endl;
-    /////////////////////////BDADODS
+	
+	cout << endl;
+	/////////////////////////BDADODS
 
 	 string utilizador = "B6_5";  
 	 string palavra = "B6_5"; 
@@ -76,65 +50,4 @@ int main(void)
 		 cerr << "Erro: " << erro.getMessage () << endl; 
 	 }   cin.get();  
 	 return 0; 
-}
-
-
-
-
-
-
-void menuprincipal(){
-    
-    cout << "=========================" << endl << "| Welcome to UC Manager! |"  << endl << "=========================" << endl << endl;
-    
-	bool flag=false;
-	
-	int op;
-	do{
-		clear();
-		cout << " Trabalho Pratico - ESINF 2013 \n" << endl<<endl;
-		cout << " |_______________ MENU______________"<<endl;
-		cout << " 1: In development " <<endl;
-		cout << " 2: In development "<<endl;
-		cout << " 3: In development     "<<endl;
-		cout << " 0: Sair " <<endl;
-		cout << " |__________________________________|"<<endl;
-		cin>>op;
-		switch (op)
-		{
-			case 0:
-				exit(1);
-			case 1:
-				flag=true;
-				clear();
-            {
-                // INSERT FUNCTION HERE
-                cout <<endl;
-            }
-				pause();
-				clear();
-				break;
-                
-			case 2:
-				clear();
-				// INSERT FUNCTION HERE
-				pause();
-				break;
-			case 3:
-            {
-                // INSERT FUNCTION HERE
-				break;
-            }
-			default:
-            {
-				cout << "Opcao invalida!" << endl << endl;
-				
-				pause();
-				
-            }
-        }
-		
-	}while(op!=0);
-    
-	pause();
 }
