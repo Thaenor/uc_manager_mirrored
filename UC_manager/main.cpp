4d72d3f584106be0c81057b044b7ffeb89421801
1040307ivopadrao <1040307@isep.ipp.pt>
2014-01-08 16:26:18 +0000

    Afonso Reis , Ivo Padrão --- Menu

--- UC_manager/main.cpp
+++ UC_manager/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
-
+#include <stdlib.h>
+#include <stdio.h>
 #include"Pessoa.h"
 #include"Bdados.h"
 #include"Aluno.h"
@@ -12,120 +13,164 @@ using namespace std;
 int main(void)
 {
 
-    cout << "Development test area!" << endl;
- 
-    
-    cout << endl;
-    /////////////////////////BDADODS
-
-	 string utilizador = "B6_5";  
-	 string palavra = "xico"; 
-	 string bd = "193.136.62.27:1521/pdborcl"; //ou em vez de ip, gandalf   
-	 try {     cout << "Exemplo de ligacao: a ligar ..."        << endl;    
-	 BDados *ligacao = new BDados (utilizador, palavra, bd); 
-	 cout << "ligaçao estabelecida" << endl;
-	 delete ligacao;
+	cout << "Development test area!" << endl;
 
 
-	// int opcao;
-	// string sigla;
-	// char user;
-	// cin >> sigla;/*ligacao->Login(username,password);*/
+	cout << endl;
+	/////////////////////////BDADODS
 
-	// Utilizador doc=ligacao->getDocente(sigla);
-	// user=doc.getTipo();
-	// if (user=='R')
-	// {
-	//		cout << "Bem Vindo =)"<< endl<<endl;
-	//		do{
-	//		cout << "escolha uma das seguintes opcoes:" << endl<< endl ;
-	//		cout << "1. Consultar Mensagens InBox" << endl;
-	//		cout << "2. Enviar Mensagem/Pedido" << endl;
-	//		cout << "3. Marcar Reuniao" << endl;
-	//		cout << "4. Marcar Teste" << endl;
-	//		cout << "0. Logout" <<endl;
-	//		cin >> opcao;
+	string utilizador = "B6_5";
+	string palavra = "xico";
+	string bd = "193.136.62.27:1521/pdborcl"; //ou em vez de ip, gandalf   
+	try {
+		cout << "Exemplo de ligacao: a ligar ..." << endl;
+		BDados *ligacao = new BDados(utilizador, palavra, bd);
+		cout << "ligaçao estabelecida" << endl;
+		delete ligacao;
 
-	//		
-	//			switch(opcao)
-	//			{
-	//			case 1:
-	//				cout << opcao << endl;
-	//				ligacao->CarregarMsg(doc);
-	//				break;
-
-	//			case 2:
-	//				cout << opcao << endl;
-	//				ligacao->EscreveMsg();
-	//				break;
-
-	//			case 3:
-	//				cout << opcao << endl;
-	//				//ligacao->MarcaReuniao();
-	//				break;
-	//			case 4:
-	//				cout << opcao << endl;
-	//				//ligacao->MarcaTeste();
-	//				break;
-	//			case 0:
-	//				break;
-	//			default:
-	//				cout << "Opcao Incorreta"<< endl;
-	//			}
-	//		}while(opcao !=0);
-	// }
-
-	// else if(user =='D')
-	// {
-	//	 cout << "Docente login"<< endl<<endl;
-	//	do{
-	//		cout << "Bem Vindo =)"<< endl << endl << "escolha uma das seguintes opcoes:" << endl ;
-	//		cout << "1. Consultar Mensagens InBox" << endl;
-	//		cout << "2. Enviar Mensagem/Pedido" << endl;
-	//		cout << "3. Marcar Teste" << endl;
-	//		cout << "4. Marcar Apresentacao/Defesa de Trabalho Pratico" << endl;
-	//		cout << "0. Logout" <<endl;
-	//		cin >> opcao;
-	//		
-	//		
-	//			switch(opcao)
-	//			{
-	//			case 1:
-	//				ligacao->CarregarMsg();
-	//				cout << opcao << endl;
-	//				break;
-
-	//			case 2:
-	//				ligacao->EscreveMsg();
-	//				cout << opcao << endl;
-	//				break;
-
-	//			case 3:
-	//				cout << opcao << endl;
-	//				//ligacao->MarcaTeste();
-	//				break;
-	//			case 4:
-	//				cout << opcao << endl;
-	//				//ligacao->MarcaReuniao();
-	//				break;
-	//			case 0:
-	//				break;
-	//			}
-	//		}while(opcao !=0);
-	// }
-	//delete (ligacao);     cout << "Exemplo de ligacao: terminado" << endl;   
-	 }   catch (SQLException erro) {    
-		 cerr << "Erro: " << erro.getMessage () << endl; 
-	 } 
-	 
-	 void menu()
-	 {
-
-	 }
-	return 0; 
+	}
+	catch (SQLException erro) {
+		cerr << "Erro: " << erro.getMessage() << endl;
 	}
 
+	return 0;
+}
 	 
+	 void janelaLogin()
+	 {
+		 cout << "######## UC Manager ########" << endl << endl;
+		 cout << "Digite nome de utilizador: ";
+		 string _user;
+		 string _pw;
+		 cin  >> _user;
+		 cout << endl << "Password: ";
+		 cin >> _pw;
+	 }
+
+	 void janelaBV_Regente() //janela boas vindas
+	 {
+		 system("cls");
+		 cout << "---Bem-vindo--" << endl << endl <<
+			 "1-Entrar UC" << endl << "2-Adicionar UC" << endl << "3-Remover UC" << endl << "A sua opcao:" << endl;
+		 char opc;
+		 cin >> opc;
+		 if (opc > '3' || opc < '0')
+		 {
+			 janelaBV_Regente();
+		 }
+	 }
+
+	 void janelaGerirConteudo()
+	 {
+		 char opc;
+		 system("cls");
+		 cout << "-----Gerir Conteudos-----" << endl << endl << "0-Voltar" << endl <<
+			 "1-Gerir Eventos" << endl << "2-Gerir Docentes" << endl << "3-Gerir UC" << endl << "4-Gerir Alunos" << endl << "A susa opcao: " << endl;
+		 if (opc == '0')
+		 {
+			 janelaBV();
+		 }
+		 if (opc == '4' || opc == '2' || opc == '3')
+		 {
+			 // abre janela
+		 }
+		 if (opc == '1')
+		 {
+
+		 }
+	 }
+
+		 void janelaGerirEventos()
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Gerir Eventos-----" << endl << endl << "0-Voltar" << endl <<
+				 "1-Alterar" << endl << "2-Remover" << endl << "3-Adicionar" << endl << "A sua opcao: " << endl;
+			 if (opc == '0')
+			 {
+				 janelaGerirConteudo();
+			 }
+		}
+
+		 void janelaUC()
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Janela UC-----" << endl << endl << "0-Voltar" << endl <<
+				 "Leccionadas" << endl << "Permissões/Histórico" << endl;
+			 if (opc == '0')
+			 {
+				 // volta atras
+			 }
+		 }
+		 void janelaMensagens()
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Mensagens-----" << endl << endl << "0-Voltar" << endl <<
+				 "1-Caixa de entrada" << endl << "2-Escrever mensagem" << endl << "A sua opcao: " << endl;
+			 if (opc == '0')
+			 {
+				 janelaUC();
+			 }
+		 }
+		 
+		 void janelaDisciplina()
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Gestão Disciplina-----" << endl << endl << "0-Voltar" << endl <<
+				 "1-Marcar Avaliacao" << endl << "2-Publicar notas" << endl << "3-Visualizar notas" << endl << "4-Escrever sumarios" << endl  << "5-Visualizar Sumarios" 
+				 << endl << "6-Marcar aula extra" <<endl<< "7-Histórico"<<endl<< "A sua opcao: " << endl;
+			 if (opc == '0')
+			 {
+				 janelaUC();
+			 }
+		 }
+
+		 void janelaVisualizacao()
+
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Visualizacao-----" << endl << endl << "0-Voltar" << endl <<
+				 "1-Visualizar notas" << endl << "2-Visualizar sumarios" << endl << "A sua opcao: " << endl;
+			 if (opc == '0')
+			 {
+				 janelaUC();
+			 }
+		 }
+
+		 void janelaCaixaEntrada() // caixa de entrada de mensagens
+
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Caixa de entrada-----" << endl;
+			 // falta completar
+		 }
+
+		 void janelaEnviarMensagem() // enviar mensagem
+
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Enviar mensagem-----" << endl;
+			 // falta completar
+		 }
+		
+
+		 void janelaMensagemRecebida() // ler mensagem
+
+		 {
+			 char opc;
+			 system("cls");
+			 cout << "-----Mensagem-----" << endl;
+			 // falta completar
+		 }
+
+
+
 
 
 
